0c30641bf804a699e2afa69768a01bfe11d4045d
Mohd Kaif <155153843+Mohdkaif07@users.noreply.github.com>
2025-04-21 23:34:36 +0530

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,12 @@
-// db/connection.js
 const mysql = require('mysql2');
+require('dotenv').config();
+
 const pool = mysql.createPool({
-  host: '127.0.0.1',
-  user: 'root',
-  password: 'kaif',
-  database: 'school_database'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
+
 module.exports = pool.promise();
